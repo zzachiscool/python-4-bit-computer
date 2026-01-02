105d4cc8f756d8743c98b3adbebc6e51eb98a430
zzach <156044882+zzachiscool@users.noreply.github.com>
2026-01-01 19:54:21 -0800

    Update proccessing.py
    Added a call to the binary function using the first four elements of tokensArray.

--- proccessing.py
+++ proccessing.py
@@ -5,9 +5,9 @@ import numpy as np
 fileFind=input("what is the name of the file you need(dont say the extention)")
 file = open(fileFind+".fbb", "r")
 content = file.read()
-print(content)
 file.close()
 tokens = re.findall(r"1|0",content)
 tokensArray=np.array(tokens, dtype=int)
 print(tokensArray)
+
 binary(tokensArray[0],tokensArray[1],tokensArray[2],tokensArray[3])
