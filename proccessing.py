21d8cc96048d40dd5d321311d5ff9703956747c4
zzach <156044882+zzachiscool@users.noreply.github.com>
2026-01-01 21:59:30 -0800

    fixed some stuff

--- proccessing.py
+++ proccessing.py
@@ -1,13 +1,27 @@
-#gets a file with .fbb at the end
 import re
 from logic import *
 import numpy as np
-fileFind=input("what is the name of the file you need(dont say the extention)")
+parsing=False
+fileFind=input("what is the name of the file you want")
 file = open(fileFind+".fbb", "r")
 content = file.read()
 file.close()
 tokens = re.findall(r"1|0",content)
 tokensArray=np.array(tokens, dtype=int)
 print(tokensArray)
-
-binary(tokensArray[0],tokensArray[1],tokensArray[2],tokensArray[3])
+parsing=True
+iZero=0
+iOne=1
+iTwo=2
+iThree=3
+while parsing:
+    binary(tokensArray[iZero],tokensArray[iOne],tokensArray[iTwo],tokensArray[iThree])
+    print("working")
+    iZero+=4
+    iOne+=4
+    iTwo+=4
+    iThree+=4
+    if iThree>tokensArray.size:
+        break
+    else:
+        continue
