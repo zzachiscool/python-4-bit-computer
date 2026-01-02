3e2045aeffd53045c921c4b98e7ed5184f0b9259
zzach <156044882+zzachiscool@users.noreply.github.com>
2026-01-01 17:42:25 -0800

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,8 @@
+#proceses binary
+def binary(digZero,digOne,digTwo,digThree):
+    zero=digZero*2**0
+    one=digOne*2**1
+    two=digTwo*2**2
+    three=digThree*2**3
+    all=zero+one+two+three
+    print(all)
